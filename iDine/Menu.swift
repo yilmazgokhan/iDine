128e3bb750a950a215b0da08d8c6469490cab698
yilmazgokhan <yilmaz_gokhan@yahoo.com>
2022-09-25 23:55:36 +0300

    Change row ui

--- iDine/Menu.swift
+++ iDine/Menu.swift
@@ -21,16 +21,16 @@ struct MenuItem: Codable, Equatable, Identifiable {
     var price: Int
     var restrictions: [String]
     var description: String
-
+    
     var mainImage: String {
         name.replacingOccurrences(of: " ", with: "-").lowercased()
     }
-
+    
     var thumbnailImage: String {
         "\(mainImage)-thumb"
     }
-
-    #if DEBUG
+    
+#if DEBUG
     static let example = MenuItem(id: UUID(), name: "Maple French Toast", photoCredit: "Joseph Gonzalez", price: 6, restrictions: ["G", "V"], description: "Sweet, fluffy, and served piping hot, our French toast is flown in fresh every day from Maple City, Canada, which is where all maple syrup in the world comes from. And if you believe that, we have some land to sell you…")
-    #endif
+#endif
 }
