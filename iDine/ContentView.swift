b03c0a863c20909ffed6cb99246ccca28e0538e6
yilmazgokhan <yilmaz_gokhan@yahoo.com>
2022-09-25 16:04:55 +0300

    Create list view

--- iDine/ContentView.swift
+++ iDine/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List{
+                ForEach(menu) { section in
+                    Section(header: Text(section.name)) {
+                        ForEach(section.items) { item in
+                            Text(item.name)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Menu")
+            .listStyle(GroupedListStyle())
+        }
     }
 }
 
---
+++ iDine/ItemRow.swift
@@ -0,0 +1,20 @@
+//
+//  ItemRow.swift
+//  iDine
+//
+//  Created by Gökhan Yılmaz on 25.09.2022.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRow()
+    }
+}
