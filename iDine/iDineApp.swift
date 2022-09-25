9578baa2cb00b902c4c27676d47d8114b289e923
yilmazgokhan <yilmaz_gokhan@yahoo.com>
2022-09-25 15:50:26 +0300

    Create project

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Gökhan Yılmaz on 25.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
